cde7e1e9adde9a4658848ace8e00db5f0c6f2d27
asier-acuna <asier.acuna@rai.usc.es>
2025-02-21 23:09:29 +0100

    extension fija derecha

---
+++ iter2/panel.js
@@ -0,0 +1,10 @@
+function getQueryParam(param) {
+    const urlParams = new URLSearchParams(window.location.search);
+    return urlParams.get(param);
+  }
+  
+  const imgUrl = getQueryParam('img');
+  document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('imgUrl').textContent = imgUrl || 'No se encontró la URL de la imagen.';
+    console.log('URL de imagen recibida:', imgUrl);
+  });
